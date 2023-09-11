36f0136c83d6b018c3b4b341fd1beda80e142ed3
hvm geeks <hvmgeeks@gmail.com>
2023-09-11 13:40:06 +0300

    latest

--- server/server.js
+++ server/server.js
@@ -14,17 +14,6 @@ app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute);
 const port = process.env.PORT || 5000;
 
-const path = require("path");
-__dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client" , "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });   
-} 
-
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
