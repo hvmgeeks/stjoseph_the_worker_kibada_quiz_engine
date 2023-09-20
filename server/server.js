f79836725b4002a58ca19fd7e5b566d39e0fa8b5
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-20 16:38:12 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const winston = require("winston");
 const app = express();
 require("dotenv").config();
+
 app.use(express.json());
+
+// Winston L Conf
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'stjosephservice' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
 const dbConfig = require("./config/dbConfig");
 
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
-const resportsRoute = require("./routes/reportsRoute");
-
+const reportsRoute = require("./routes/reportsRoute");
 
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
-app.use("/api/reports", resportsRoute);
+app.use("/api/reports", reportsRoute);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  logger.info(`Server listening on port ${port}`);
 });
