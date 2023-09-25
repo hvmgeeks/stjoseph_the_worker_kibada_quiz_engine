f68e85a67971cb226ceeda6b4acb532a15ca0d6a
@Ansigar <72963613+Anzigar@users.noreply.github.com>
2023-09-25 08:12:13 +0300

    Update server.js
    --

--- server/server.js
+++ server/server.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 
 app.use(express.json());
 
+app.use(cors());
 const dbConfig = require("./config/dbConfig");
 
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const reportsRoute = require("./routes/reportsRoute");
 
-const port = process.env.PORT || 5000;
-
-// app.use((req, res, next) => {
-//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-//   next();
-// });
-
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", reportsRoute);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
